ccc98ad9a5d94f2ae5f9a522ebc0a9cc4be4f559
RafDev88 <rafaelsousa.dev@hotmail.com>
2025-05-17 21:45:13 -0300

    feat: initialize Quasar project with Supabase integration
    - Added PostCSS configuration for autoprefixing.
    - Included favicon and various icon sizes in the public directory.
    - Created main configuration file for Quasar.
    - Set up main application structure with App.vue and layouts.
    - Implemented essential components and pages including Login and ErrorNotFound.
    - Established routing with a dedicated routes file.
    - Integrated Axios for API requests in the boot directory.
    - Developed composable for user authentication with Supabase.
    - Added global CSS file for application styling.

---
+++ Documents/EstoqueSupaBase/quasar-estoque-supabase/src/router/routes.js
@@ -0,0 +1,21 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', name: 'loginDefault', component: () => import('pages/Login.vue') },
+      { path: 'login', name: 'login', component: () => import('pages/Login.vue') }
+
+    ]
+
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
